1fad5e0ac8e8cfd7471893dca383ad68aa6aab74
Vikas Singh <xanderbilla@gmail.com>
2024-03-10 10:02:02 +0530

    U4# Initaialized Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="">
-      <Image
-          src="/img/logo.png"
-          alt="Logo"
-          width={300}
-          height={300}
-        />
+        <Image src="/img/logo.png" alt="Logo" width={300} height={300} />
       </div>
       <div className="text-4xl">Welcome! to the Journey on</div>
       <div className="flex items-center justify-center gap-4">
+        <Image
+          src="/img/auth-logo.png"
+          alt="Auth Logo"
+          width={100}
+          height={300}
+        />
+        <span className="text-xl">➕</span>
         <Image
           src="/img/next-logo.png"
           alt="Next Logo"
